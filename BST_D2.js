6099f950465822aa3e5b4630fd2902918f5ffd10
Saeng su Kwon <83226593+skwon8@users.noreply.github.com>
2022-04-12 12:11:26 -0400

    Add files via upload

---
+++ BST_D2.js
@@ -0,0 +1,110 @@
+class BSTNode {
+    constructor(value) {
+        this.left = undefined;
+        this.right = undefined;
+        this.value = value;
+    }
+};
+
+class BST {
+    constructor() {
+        this.root = undefined;
+    }
+
+    Min() {
+        var runner = this.root
+        while (runner.left != undefined) {
+            runner = runner.value
+        }
+        while (runner.left != null) {
+            runner = runner.left
+        }
+        return runner.value
+    }
+
+    Max() {
+        var runner = this.root
+        if (runner.right == undefined) {
+            return runner
+        }
+        while (runner.right != null) {
+            runner = runner.right
+        }
+        return runner.value
+    }
+
+
+    /* Determine whether the given value exists within the tree */
+    Contains(value) {
+        var runner = this.root
+
+        if (value = runner.value) {
+            console.log("We found value")
+            return this
+        }
+        while (runner.left || runner.right != undefined) {
+            if (value < runner.value) {
+                runner = runner.left
+            }
+            else {
+                runner = runner.right
+            }
+            if (value < runner.value) {
+                console.log("value found in Tree!")
+                return this
+            }
+        }
+    };
+
+    /* Determine whether the given value exists within the tree RECURSIVELY */
+    ContainsRecursive(node, value) {
+        if (!node) return false;
+
+        if (node.value == value) {
+            return true;
+        } else {
+            if (value < node.value) {
+                return this.ContainsRecursive(node.left, value);
+            } else {
+                return this.ContainsRecursive(node.right, value);
+            }
+        }
+    }
+
+
+        /* I'm just here to print your BST In-Order. */
+        /* Could use me as an example to solve above algos, since I'm recursive and all. */
+    Log(node, fmt = "") {
+        if (!node) return fmt;
+        fmt = this.Log(node.left, fmt);
+        fmt += node.value + "->";
+        fmt = this.Log(node.right, fmt);
+        return fmt;
+    }    
+    
+};
+
+
+
+/*****************************************************************************/
+/* Code Tests Below */
+/*****************************************************************************/
+let bst = new BST();
+
+bst.root = new BSTNode(100);
+bst.root.left = new BSTNode(50);
+bst.root.right = new BSTNode(150);
+
+bst.root.left.left = new BSTNode(25);
+bst.root.left.right = new BSTNode(75);
+
+bst.root.right.left = new BSTNode(125);
+bst.root.right.right = new BSTNode(175);
+
+// console.log(bst.Log(bst.root));           /* Uncomment me if you want to see your tree */
+console.log(bst.Min())                      /* Expected: 25 */
+console.log(bst.Max())                      /* Expected: 175 */
+console.log(bst.Contains(200))                  /* Expected: false */
+console.log(bst.Contains(125))                  /* Expected: true */
+console.log(bst.ContainsRecursive(bst.root, 15));   /* Expected: false */
+console.log(bst.ContainsRecursive(bst.root, 75));   /* Expected: true */
